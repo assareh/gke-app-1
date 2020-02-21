7dcc570b81971e051e878ed48a613083f5ea1437
assareh <assareh@hashicorp.com>
2020-02-20 17:02:11 -0800

    removed palace arcade

--- palacearcade.tf
+++ palacearcade.tf
@@ -1,71 +1,71 @@
 
-resource "kubernetes_service" "palacearcade" {
-  metadata {
-    namespace = kubernetes_namespace.staging.metadata.0.name
-    name      = "palacearcade"
-  }
+// resource "kubernetes_service" "palacearcade" {
+//   metadata {
+//     namespace = kubernetes_namespace.staging.metadata.0.name
+//     name      = "palacearcade"
+//   }
 
-  spec {
-    selector = {
-      app = "palacearcade"
-    }
+//   spec {
+//     selector = {
+//       app = "palacearcade"
+//     }
 
-    port {
-      protocol    = "TCP"
-      port        = 80
-      target_port = 80
-    }
+//     port {
+//       protocol    = "TCP"
+//       port        = 80
+//       target_port = 80
+//     }
 
-    type             = "LoadBalancer"
-    load_balancer_ip = google_compute_address.palacearcade.address
-  }
-}
+//     type             = "LoadBalancer"
+//     load_balancer_ip = google_compute_address.palacearcade.address
+//   }
+// }
 
-resource "kubernetes_deployment" "palacearcade" {
-  metadata {
-    name = "palacearcade"
-    namespace = kubernetes_namespace.staging.metadata.0.name
-    labels = {
-      app = "palacearcade"
-    }
-  }
+// resource "kubernetes_deployment" "palacearcade" {
+//   metadata {
+//     name = "palacearcade"
+//     namespace = kubernetes_namespace.staging.metadata.0.name
+//     labels = {
+//       app = "palacearcade"
+//     }
+//   }
 
-  spec {
-    replicas = 3
+//   spec {
+//     replicas = 3
 
-    selector {
-      match_labels = {
-        app = "palacearcade"
-      }
-    }
+//     selector {
+//       match_labels = {
+//         app = "palacearcade"
+//       }
+//     }
 
-    template {
-      metadata {
-        labels = {
-          app = "palacearcade"
-        }
-      }
+//     template {
+//       metadata {
+//         labels = {
+//           app = "palacearcade"
+//         }
+//       }
 
-      spec {
-        container {
-          image = "scarolan/palacearcade"
-          name  = "palacearcade"
-        }
-      }
-    }
-  }
+//       spec {
+//         container {
+//           image = "scarolan/palacearcade"
+//           name  = "palacearcade"
+//         }
+//       }
+//     }
+//   }
 
-  provisioner "local-exec" {
-    when    = destroy
-    command = "sleep 10"
-  }
-}
+//   provisioner "local-exec" {
+//     when    = destroy
+//     command = "sleep 10"
+//   }
+// }
 
-resource "google_compute_address" "palacearcade" {
-  name   = "${var.gcp_project}-palacearcade"
-  region = var.gcp_region
-}
+// resource "google_compute_address" "palacearcade" {
+//   name   = "${var.gcp_project}-palacearcade"
+//   region = var.gcp_region
+// }
 
-output "palace-load-balancer-ip" {
-  value = "${google_compute_address.palacearcade.address}"
-}
+// output "palace-load-balancer-ip" {
+//   value = "${google_compute_address.palacearcade.address}"
+// }
