88e5a6b1e8364ac6f7c8160a637cf7f80af188f9
assareh <assareh@hashicorp.com>
2020-02-20 16:17:08 -0800

    brace typo and tweaks

--- main.tf
+++ main.tf
@@ -65,8 +65,6 @@ resource "kubernetes_service" "nginx" {
       run = "nginx"
     }
 
-    session_affinity = "ClientIP"
-
     port {
       protocol    = "TCP"
       port        = 80
@@ -83,7 +81,7 @@ resource "kubernetes_deployment" "example" {
     name = "nginx"
     namespace = kubernetes_namespace.staging.metadata.0.name
     labels = {
-      test = "nginx"
+      app = "nginx"
     }
   }
 
@@ -92,47 +90,21 @@ resource "kubernetes_deployment" "example" {
 
     selector {
       match_labels = {
-        test = "nginx"
+        app = "nginx"
       }
     }
 
     template {
       metadata {
         labels = {
-          test = "nginx"
+          app = "nginx"
         }
       }
 
       spec {
         container {
-          image = "nginx:latest"
+          image = "nginx:1.7.9"
           name  = "nginx"
-
-          resources {
-            limits {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/nginx_status"
-              port = 80
-
-              http_header {
-                name  = "X-Custom-Header"
-                value = "Awesome"
-              }
-            }
-
-            initial_delay_seconds = 3
-            period_seconds        = 3
-          }
         }
       }
     }
